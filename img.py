36bc8ca6455963164acb518b390e8d275133bc74
Parikshith Murthy <miloismypetdog@gmail.com>
2022-08-30 20:34:23 +0530

    monke

---
+++ img.py
@@ -0,0 +1,10 @@
+import sys # to access the system
+import cv2
+img = cv2.imread("monk.jpg", cv2.IMREAD_ANYCOLOR)
+ 
+while True:
+    cv2.imshow("Monke", img)
+    cv2.waitKey(0)
+    sys.exit()
+ 
+cv2.destroyAllWindows()
